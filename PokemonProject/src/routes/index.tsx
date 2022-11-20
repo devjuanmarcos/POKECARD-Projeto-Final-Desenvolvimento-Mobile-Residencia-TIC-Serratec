4b0409904a46127777c968d87253de4690276fc0
Juan Marcos de Souza Texe <111091371+devjuanmarcos@users.noreply.github.com>
2022-11-20 15:52:50 -0300

    componentizando BotaoVertical

--- PokemonProject/src/routes/index.tsx
+++ PokemonProject/src/routes/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import {TouchableOpacity, Text} from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer, StackRouter } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import styles from './styles'
+
 import Home from '../screens/Home';
+import ButtonVertical from '../components/BotaoVertical';
 
 const Tab = createBottomTabNavigator<RootTabParamList>();
 const Stack = createNativeStackNavigator()
@@ -13,14 +12,7 @@ const Stack = createNativeStackNavigator()
 export type RootTabParamList = {
     Home: {token:string};
 }
-const ButtonVertical = ({icon, text}) => {
-    return (
-        <TouchableOpacity style={styles.button}>
-        <Icon name={icon} color="#fff" size={30} />
-        <Text style={styles.text}>{text}</Text>
-        </TouchableOpacity>
-    );
-}
+
 const TabNavigator = () =>{
     return(
     <Tab.Navigator
@@ -32,23 +24,12 @@ const TabNavigator = () =>{
                 tabBarStyle: { backgroundColor: "#000", paddingBottom: 1 }
             }}
         >
-            <Tab.Screen
-                name='Home'
-                component={Home}
-                options={{
+            <Tab.Screen name='Home' component={Home} options={{
                     tabBarIcon: ({ color }) => {
                         return (
-                            <ButtonVertical 
-                                icon="home" text={Home}                                
-                            />
-                            
-                        )
-                    }
-                }}
-            />
-        </Tab.Navigator>
-    )
-}
+                            <ButtonVertical icon="home" text={Home}/>)}}}/>
+        </Tab.Navigator>)}
+        
 export function Routes() {
     return (
         <NavigationContainer>
--- PokemonProject/src/routes/styles.ts
+++ PokemonProject/src/routes/styles.ts
@@ -1,15 +1,7 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    text: {
-    
-        color: '#fff'
-    },
-    button: {
-        alignItems: 'center',
-        justifyContent: 'center',
-
-    }
+   
 })
 
 export default styles;
