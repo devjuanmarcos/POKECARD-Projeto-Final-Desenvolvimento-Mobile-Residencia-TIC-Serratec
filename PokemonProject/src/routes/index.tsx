d3a476fdb412d7775a1b503ecd5eb953c7328a51
RaquelAlvesBarbosa <kellzinha_vipe@hotmail.com>
2022-11-23 18:54:00 -0300

    Configurando Routes

--- PokemonProject/src/routes/index.tsx
+++ PokemonProject/src/routes/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from "@react-navigation/native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons"
 
 import ImageComp from "../components/ImageComp";
 import { setStatusBarBackgroundColor } from "expo-status-bar";
@@ -15,69 +15,79 @@ const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator<RootTabParamList>();
 
 export type RootTabParamList = {
-  Home: { token: string };
+  home: { token: string };
   Cartas: undefined;
   Carrinho: undefined;
-};
+}
+export function Routeshome() {
+
+  return (
+     <Tab.Navigator
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: "#fff",
+                tabBarInactiveTintColor: "#aaa",
+                tabBarInactiveBackgroundColor: "#000",
+                tabBarStyle: { backgroundColor: "#000", paddingBottom: 2 ,height:50}
+            }}
+        >
+            <Tab.Screen
+                name='home'
+                component={Home}
+                options={{
+                    tabBarIcon: ({ color }) => {
+                        return (
+                            <MaterialCommunityIcons
+                                resizeMode='contain'
+                                name="home"
+                            />
+                        )
+                    }
+                }}
+            />
+            <Tab.Screen
+                name="Cartas"
+                component={Pokemon}
+                options={{
+                    tabBarIcon: ({ color }) => {
+                      return (
+                        <MaterialCommunityIcons
+                          resizeMode='contain'
+                          name="cards"
+                        />
+                      )
+                    }
+                }}
+            />
+        </Tab.Navigator>
+
+  );
+            }
 
 export function Routes() {
+
   return (
     <NavigationContainer>
-      {/* <Stack.Navigator
+      <Stack.Navigator
         screenOptions={{
           headerStyle: {
             backgroundColor: "red",
           },
           headerTintColor: "white",
         }}
-        initialRouteName="Tela Inicial"
+        initialRouteName="Login"
       >
         <Stack.Screen
-          options={{ headerShown: true }}
-          name="Tela Inicial"
-          component={MeuComponente}
-        />
-        
-        <Stack.Screen name="Imagem" component={ImageComp} />
-        <Stack.Screen name="Loja" component={Pokemon} />
-
-      </Stack.Navigator> */}
-      <Tab.Navigator
-        screenOptions={{
-          headerShown: false,
-          tabBarActiveTintColor: "#fff",
-          tabBarInactiveTintColor: "#aaa",
-          tabBarInactiveBackgroundColor: "#000",
-          tabBarStyle: {
-            backgroundColor: "#000",
-            paddingBottom: 2,
-            height: 50,
-          },
-        }}
-      >
-        <Tab.Screen
-          name="Home"
+          options={{ headerShown: false }}
+          name="Login"
           component={Login}
-          options={{
-            tabBarIcon: ({ color }) => {
-              return (
-                <MaterialCommunityIcons resizeMode="contain" name="home" />
-              );
-            },
-          }}
         />
-        <Tab.Screen
-          name="Cartas"
-          component={Pokemon}
-          options={{
-            tabBarIcon: ({ color }) => {
-              return (
-                <MaterialCommunityIcons resizeMode="contain" name="cards" />
-              );
-            },
-          }}
-        />
-      </Tab.Navigator>
+
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen  options={{ headerShown: false }} name="Home" component={Routeshome} />
+
+      </Stack.Navigator> 
     </NavigationContainer>
+
   );
 }
