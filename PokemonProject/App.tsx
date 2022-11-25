7e80457603899082ac532fd3650678fd413851e5
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2022-11-25 12:56:14 -0300

    ...

--- PokemonProject/App.tsx
+++ PokemonProject/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { StatusBar } from "react-native";
+import { ProvedorCarrinho } from "./src/Context/CarrinhoContexto";
 import { Routes } from "./src/routes";
 
 export default function App  () {
 
-  return (
-
+  return (    
+  <ProvedorCarrinho>
+    <StatusBar 
+    hidden={false}
+    translucent={true}
+    />
     <Routes/>
 
+  </ProvedorCarrinho>
   );
 }
